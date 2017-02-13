1efa03a2ee71826ceb505c2b66dbcc9945597b07
psloss <psloss@belltec.com>
2017-02-13 10:55:35 -0600

    * Initial commit

---
+++ PlaneLog/PlaneLog/Models/Plane.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PlaneLog.Models
+{
+    public class Plane
+    {
+        public int Id { get; set; }
+        public string TailNumber { get; set; }
+        public string Manufacturer { get; set; }
+        public string Model { get; set; }
+    
+    }
+}
